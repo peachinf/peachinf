908f4bc7c000658a06eae2edf8f26356e48d6d68
peachinf <peachinf@gmail.com>
2026-01-18 12:29:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+const express = require('express'); const fetch = require('node-fetch');
+const app = express();
+app.get('/test', async (req,res)=>res.send(await (await fetch(req.query.url)).text()));
+app.listen(process.env.PORT || 8080);
